9e84fffe2b2164c7692fc640a195b5e5d21f7ad5
rafaelpardor <rafaelpardor@protonmail.com>
2019-11-20 00:05:24 -0500

    Flow statements

---
+++ cplatzi/03-ifstatement.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main()
+{
+	int a=1;
+	if (a==1){
+		printf("a es igual a %d\n", a);
+	}else{
+		puts("Che loco algo anda mal");
+	}
+ }
---
+++ cplatzi/04-switchstatement.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int main()
+{
+	int a = 1;
+	switch(a){
+		case 1:
+			a = 10;
+			printf("%d\n",a);
+			break;
+		case 2:
+			a= 20;
+			printf("%d\n",a);
+			break;
+		case 3:
+			a=30;
+			printf ("%d\n",a);
+			break;
+		default:
+			a=100;
+			printf ("%d\n",a);
+			break;
+	}
+}
