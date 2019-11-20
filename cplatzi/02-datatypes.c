22dff5618a42ee35e3684e41cc7e2165acea26e3
rafaelpardor <rafaelpardor@protonmail.com>
2019-11-19 23:02:07 -0500

    Data types

---
+++ cplatzi/02-datatypes.c
@@ -0,0 +1,24 @@
+// int
+// float
+// char
+// String
+// bool 0 = false - 1=true
+
+#include<stdio.h>
+
+int main()
+{
+	int myNumber;
+	float myFloat = 3.14;
+	char letter = 'R';
+
+	myNumber = 20;
+	printf("%d\n",myNumber);
+
+	myNumber = myNumber + 2;
+	printf("%d\n\n",myNumber);
+	printf("%f\n",myFloat);
+	printf("%d\n",letter);
+
+
+}
