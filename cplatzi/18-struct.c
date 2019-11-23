96d8116b01be5c4434804d96ebaa45f923a2dfcd
rafaelpardor <rafaelpardor@protonmail.com>
2019-11-23 01:33:01 -0500

    Structs

---
+++ cplatzi/18-struct.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+
+int main(int argc, const char * argv[])
+{
+	struct {
+		char name[100];
+		int age;
+	} person;
+
+	if(argc < 3){
+		printf("Indique nombre y edad por favor\n");
+		return 1;
+	}
+	if(strlen(argv[1]) < 100){
+		strcpy(person.name, argv[1]);
+	}
+	person.age = atoi(argv[2]);
+
+	printf("Nombre: %s, edad: %d.\n",person.name,person.age);
+}
