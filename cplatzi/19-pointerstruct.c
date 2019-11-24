be9ca7ca9caf883f51a7f04111359b4e5d320d77
rafaelpardor <rafaelpardor@protonmail.com>
2019-11-23 19:48:17 -0500

    Structs

--- cplatzi/19-pointerstruct.c
+++ cplatzi/19-pointerstruct.c
@@ -2,21 +2,30 @@
 #include<string.h>
 #include<stdlib.h>
 
+struct PERSON{
+	char name[100];
+	int age;
+};
+
+void fillPersonData(struct PERSON * person, const char * name, int age)
+{
+	if (strlen(name)<100){
+		strcpy(person->name,name);
+	}
+	(*person).age=age;
+}
+
 int main(int argc, const char * argv[])
 {
-	struct {
-		char name[100];
-		int age;
-	} person;
+	struct PERSON person;
 
 	if(argc < 3){
 		printf("Indique nombre y edad por favor\n");
 		return 1;
 	}
-	if(strlen(argv[1]) < 100){
-		strcpy(person.name, argv[1]);
-	}
 	person.age = atoi(argv[2]);
+
+	fillPersonData(&person, argv[1], atoi(argv[2]));
 
 	printf("Nombre: %s, edad: %d.\n",person.name,person.age);
 	return 0;
