8b09dd06b62dccef284017588f8a82ee57bbed97
rafaelpardor <rafaelpardor@protonmail.com>
2020-05-25 20:47:28 -0400

    C Programming

--- cprogramminglang/chapter-01/02-temperature.c
+++ cprogramminglang/chapter-01/02-temperature.c
@@ -2,7 +2,8 @@
 
 void main()
 {
-    int fahr, celsius;
+    printf("Temperature Program with while");
+    float fahr, celsius;
     int lower, upper, step;
 
     lower = 0;
@@ -14,8 +15,8 @@ void main()
 
     printf("F\t|\tC\n");
     while(fahr <= upper){
-        celsius = 5 * (fahr-32)/9;
-        printf("%d\t->\t%d\n", fahr, celsius);
+        celsius = (5.0 * 9.0) * (fahr-32.0);
+        printf("%3.0f\t->\t%6.1f\n", fahr, celsius);
         fahr = fahr + step;
     }
 }
---
+++ cprogramminglang/chapter-01/README.md
@@ -0,0 +1,6 @@
+# Notes
+
+char - character /  a single byte
+short - short integer
+long - long integer
+double - double precision floating point
